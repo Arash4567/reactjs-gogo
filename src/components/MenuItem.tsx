9b074ca8e4edd81048cf1b46369ef3ab255473c7
OG'ABEK <shomoyxhacker007@mail.ru>
2023-04-07 16:54:51 +0500

    feat: index layout created

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Link, NavLink } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import { ISidebarMenu } from "../models/sidebar.interface";
 
 export default function MenuItem<ReactNode>({ menu }: {
@@ -26,7 +26,7 @@ export default function MenuItem<ReactNode>({ menu }: {
             </div>
           </li>
           :
-          <NavLink to={menu.url} onClick={() => setSelectedPage(menu.url)} className="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
+          <Link to={menu.url} onClick={() => changePage(menu.url)} className="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
             <div className="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
             <div className="px-10 flex items-center text-white space-x-5">
               <div className="p-1.5">
@@ -34,7 +34,7 @@ export default function MenuItem<ReactNode>({ menu }: {
               </div>
               <p className="">{menu.name}</p>
             </div>
-          </NavLink>
+          </Link>
         }
       </BrowserRouter>
     </React.Fragment>
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,9 +4,51 @@ import { Link } from "react-router-dom";
 export default function Index<ReactNode>() {
   return (
     <React.Fragment>
-      <div>
-        INDEX
-        <Link to="/balance">Balance</Link>
+      <div className="px-10 py-3">
+        <div className="grid grid-cols-2 gap-5">
+          <div className="bg-white rounded-xl capitalize h-[40vh] p-5">
+            <div className="flex items-center">
+              <div>Spending stats</div>
+              <div className="flex items-center">
+                <div>Personal</div>
+                <div>Business</div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-5">
+            <div className="bg-white rounded-xl capitalize h-full p-5">
+              as
+            </div>
+            <div className="bg-white rounded-xl capitalize h-full p-5">
+              as
+            </div>
+            <div className="bg-white rounded-xl capitalize h-full p-5">
+              as
+            </div>
+            <div className="bg-white rounded-xl capitalize h-full p-5">
+              as
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 gap-5 mt-5">
+          <div className="bg-white rounded-xl capitalize h-[43vh] p-5 col-span-3">
+            <div className="flex items-center">
+              <div>Spending stats</div>
+              <div className="flex items-center">
+                <div>Personal</div>
+                <div>Business</div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-5 grid-rows-4">
+            <div className="bg-white rounded-xl capitalize h-full p-5 row-span-3">
+              as
+            </div>
+            <div className="bg-white rounded-xl capitalize h-full p-5">
+              as
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   )
