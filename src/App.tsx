2816468a2301e57940ff21cf234d89c9fe947a31
OG'ABEK <shomoyxhacker007@mail.ru>
2023-04-13 15:04:07 +0500

    feat: some improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
 import { useState } from "react";
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
-import DashboardLayout from "./layouts/DashboardLayout";
-import LoginLayout from "./layouts/LoginLayout";
-import { privateRouter, publicRouter } from "./router";
-import Index from "./pages/Index";
-import Balance from "./pages/Balance";
-import Login from "./pages/Login";
-import Sidebar from "./components/Sidebar";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
-
-// Create a client
-const queryClient = new QueryClient()
+import Sidebar from "./components/Sidebar";
+import Balance from "./pages/Balance";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
 
 function App() {
   const [isLogin, setIsLogin] = useState<boolean>(true)
   return (
     <>
       <BrowserRouter>
-        <div className="flex bg-gray-100 h-screen">
-          <Sidebar />
-          <div className="flex-1 ml-80">
-            <Navbar />
-            <div className="pt-5 mb-24 text-gray-900">
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/balance" element={<Balance />} />
-                <Route path="/login" element={<Login test="hjhj" />} />
-              </Routes>
+        {isLogin ? (
+          <div className="flex bg-gray-100 h-screen">
+            <Sidebar />
+            <div className="flex-1 ml-80">
+              <Navbar />
+              <div className="pt-5 mb-24 text-gray-900">
+                <Routes>
+                  <Route path="/" element={<Index />} />
+                  <Route path="/balance" element={<Balance />} />
+                  <Route path="/login" element={<Login test="test" />} />
+                </Routes>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Login test="test" />} />
+          </Routes>
+        )
+        }
       </BrowserRouter>
     </>
   )
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -8,32 +8,32 @@ export default function MenuItem<ReactNode>({ menu }: {
   const [selectedPage, setSelectedPage] = useState<string>(localStorage.getItem('currentPage') || '/')
 
   const changePage = (url: string): void => {
-    localStorage.setItem('currentPage', url)
+    // localStorage.setItem('currentPage', url)
     setSelectedPage(url)
   }
   return (
     <React.Fragment>
-        {menu.url === selectedPage ?
-          <li className="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
-            <div className="-ml-1.5 w-3 h-10 rounded-xl bg-yellow-500"></div>
-            <div className="px-10 flex items-center text-yellow-500 space-x-5">
-              <div className="bg-yellow-500/10 rounded-xl p-1.5">
-                {menu.icon}
-              </div>
-              <p className="">{menu.name}</p>
+      {menu.url === selectedPage ?
+        <li className="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
+          <div className="-ml-1.5 w-3 h-10 rounded-xl bg-yellow-500"></div>
+          <div className="px-10 flex items-center text-yellow-500 space-x-5">
+            <div className="bg-yellow-500/10 rounded-xl p-1.5">
+              {menu.icon}
             </div>
-          </li>
-          :
-          <Link to={menu.url} className="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
-            <div className="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
-            <div className="px-10 flex items-center text-white space-x-5">
-              <div className="p-1.5">
-                {menu.icon}
-              </div>
-              <p className="">{menu.name}</p>
+            <p className="">{menu.name}</p>
+          </div>
+        </li>
+        :
+        <Link to={menu.url} onClick={() => changePage(menu.url)} className="relative flex items-center h-10 w-full hover:bg-yellow-500/10 py-7 cursor-pointer transition-colors duration-300">
+          <div className="-ml-1.5 w-3 h-10 rounded-xl bg-gray-900"></div>
+          <div className="px-10 flex items-center text-white space-x-5">
+            <div className="p-1.5">
+              {menu.icon}
             </div>
-          </Link>
-        }
+            <p className="">{menu.name}</p>
+          </div>
+        </Link>
+      }
     </React.Fragment>
   )
 }
